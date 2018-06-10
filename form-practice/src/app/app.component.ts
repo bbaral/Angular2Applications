880718896f1e454fcbd5ca0aa134ffb791586bac
bbaral <bbaral@asu.edu>
2018-06-10 19:49:38 -0400

    dynamic form update after user enter

--- form-practice/src/app/app.component.ts
+++ form-practice/src/app/app.component.ts
@@ -12,8 +12,32 @@ export class AppComponent {
   answer = '';
   genders = ['male', 'female'];
 
+  user = {
+    username : '',
+    email : '',
+    secretQuestion : '',
+    answer: '',
+    gender: ''
+  };
+
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signUpForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer:  '',
+    //   gender: 'male'
+    // });
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   /**
@@ -26,6 +50,11 @@ export class AppComponent {
    */
 
   onSubmit() {
-    console.log(this.signUpForm);
+    this.submitted = true;
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
   }
 }
