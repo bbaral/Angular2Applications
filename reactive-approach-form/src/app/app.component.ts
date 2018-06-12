cd6306ba68d444170421b138e0607987e1926580
bbaral <bbaral@asu.edu>
2018-06-11 21:29:14 -0400

    reactive form restriction value test

--- reactive-approach-form/src/app/app.component.ts
+++ reactive-approach-form/src/app/app.component.ts
@@ -1,20 +1,53 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
-  SignupForm: FormGroup;
+  signupForm: FormGroup;
+  forbiddenUserName = ['max', 'andy', 'leo'];
+
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.SignupForm = new FormGroup({
-      'username': new FormControl(null),
-      'email': new FormControl(null),
+    /**
+     ******This is a reactive driven approach*********
+     this.signupForm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
       'gender': new FormControl('male')
     });
+     */
+
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
+    });
+
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUserName.indexOf(control.value)) {
+      return {'nameIsForbidden' : true };
+    }
+    return null;
   }
 }
