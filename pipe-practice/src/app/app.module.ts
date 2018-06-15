0dce08a800476d11980dfd5d338acb2f24d1ad60
bbaral <bbaral@asu.edu>
2018-06-14 23:02:33 -0400

    pipe-practice

--- pipe-practice/src/app/app.module.ts
+++ pipe-practice/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {ShortenPipe} from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortenPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
